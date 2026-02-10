9e9a0e111f1bc9e79d9936b604ee4a94fd36cf4e
Motojima <r.motojima5@gmail.com>
2026-02-10 15:03:49 +0900

    qaセクションの作成

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ jQuery("#js-drawer-icon").on("click", function (e) {
   jQuery("#js-drawer-icon").toggleClass("is-checked");
   jQuery("#js-drawer-content").toggleClass("is-checked");
 });
+
+jQuery(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+  if (jQuery(this).parent().hasClass("is-open")) {
+    jQuery(this).parent().removeClass("is-open");
+    jQuery(this).next().slideUp();
+  } else {
+    jQuery(this).parent().addClass("is-open");
+    jQuery(this).next().slideDown();
+  }
+});
